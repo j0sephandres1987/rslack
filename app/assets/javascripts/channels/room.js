5ed8daf0c1d80e79bc530761804054825411dcc7
Joseph Palmezano <infocreativosweb.co@gmail.com>
2016-08-14 16:08:40 -0500

    messages channel

---
+++ app/assets/javascripts/channels/room.js
@@ -0,0 +1,18 @@
+App.room = App.cable.subscriptions.create("RoomChannel", {
+  connected: function() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received: function(data) {
+    // Called when there's incoming data on the websocket for this channel
+    $("#messages").append("<p>"+data+"</p>");
+  },
+
+  speak: function(message) {
+    return this.perform('speak', {message: message});
+  }
+});
